34d5d0ff2457dacd2f5e17a13ca59babd0b42afe
Christian Peter <christianpb25@gmail.com>
2020-02-24 18:08:59 +0100

    Update sql.sh

--- sql.sh
+++ sql.sh
@@ -2,7 +2,7 @@
 for ((j=1;j<1000000;j++));do
 	echo '("'$j'","'${aCadenaUnica[$j]}'","'${aCadenaIndex[$j]}'","'${aCadenaIndex[$j]}'","'$(($j % 100 + 1))'"),'
 done >> datos.sql
-ec (aIdClavB (bld)
+
 
 for ((j=1;j<100;j++));do
 	echo '("'$j'","'${bCadenaUnica[$j]}'","'${aCadenaUnica[$j]}'","'${aCadenaUnica[100 - $j]}'"),'
