8a4f2305c8ce2c16f6b1d32d99efe7f68f1ab99d
Christian Peter <christianpb25@gmail.com>
2020-02-24 18:06:40 +0100

    Update sql.sh

--- sql.sh
+++ sql.sh
@@ -3,7 +3,7 @@ for ((j=1;j<1000000;j++));do
 	echo '("'$j'","'${aCadenaUnica[$j]}'","'${aCadenaIndex[$j]}'","'${aCadenaIndex[$j]}'","'$(($j % 100 + 1))'"),'
 done >> datos.sql
 echo "CREATE TABLE tabla_A (ald INT NOT NULL, aCadenaUnica VARCHAR(22) UNIQUE, aCadenaIndex VARCHAR(22), aCadenaSinIndex VARCHAR(22), aIdClaveAjena int NOT NULL, PRIMARY KEY (ald), FOREIGN KEY (aIdClaveAjena) REFERENCES tabla_B (bld));" >> tablas.sql
-echo "INSERT INTO tabla_A (ald, aCadenaUnica, aCadenaIndex, aCadenaSinIndex, aIdClaveAjena) VALUES" >> datos.sql
+echo "INSERT INTO tabla_A (ald, aCadenaUnica, aCadenaIndex, aCadenaSinIndex, aIdClaveAjena) VALUES"
 for ((j=1;j<100;j++));do
 	echo '("'$j'","'${bCadenaUnica[$j]}'","'${aCadenaUnica[$j]}'","'${aCadenaUnica[100 - $j]}'"),'
 done >> datos.sql
